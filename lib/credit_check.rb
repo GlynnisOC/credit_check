2ffbe00417bcf0d9918c4c9a2afe8faba352e2d5
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-03-07 14:52:47 -0700

    adds credit check class and test

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,7 +1,15 @@
-card_number = "5541808923795240"
 
 class Credit_check
-  
+  def each_digit
+    card_number = "5541808923795240"
+    card_number.each_char.map do |char|
+      char.to_i
+    end
+  end
+
+  def double_every_other_digit
+    each_digit.
+  end
 end
 
 # Output
